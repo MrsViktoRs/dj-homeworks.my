30cf1b23a5e0cc3f7c6929b2a44af48e48b6f06a
viktor <vhomutov167@gmail.com>
2024-01-30 13:14:33 +0300

    complete_2.0

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'logistic',
+
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +133,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
